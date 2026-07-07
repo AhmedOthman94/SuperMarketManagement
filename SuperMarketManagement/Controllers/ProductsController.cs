0deea44ce82baccc906d623f51fde04c672743da
AhmedOthman94 <othmanahmed7367@gmail.com>
2026-07-07 15:13:09 +0300

    Add Add Products

--- SuperMarketManagement/Controllers/ProductsController.cs
+++ SuperMarketManagement/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SuperMarketManagement.Models;
+using SuperMarketManagement.ViewModels;
 
 namespace SuperMarketManagement.Controllers
 {
@@ -9,6 +10,28 @@ namespace SuperMarketManagement.Controllers
 		{
 			var products = ProductsRepository.GetProducts(loadCategory: true);
 			return View(products);
+		}
+
+		public IActionResult Add()
+		{
+			var productViewModel = new ProductViewModel
+			{
+				Categories = CategoriesRepository.GetCategories()
+			};
+
+			return View(productViewModel);
+		}
+
+		[HttpPost]
+		public IActionResult Add(ProductViewModel productViewModel)
+		{
+			if (ModelState.IsValid)
+			{
+				ProductsRepository.AddProduct(productViewModel.Product);
+				return RedirectToAction(nameof(Index));
+			}
+			productViewModel.Categories = CategoriesRepository.GetCategories();
+			return View(productViewModel);
 		}
 	}
 }
